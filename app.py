a961226fd4f43d11811a4f02c8b738c7bd3826f6
SankalpGithub <sankalp2004gaikwad@gmail.com>
2023-09-09 20:17:06 +0530

    access image and apk files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from con_mongodb import con
 from bson.json_util import dumps
 from bson.objectid import ObjectId
@@ -16,21 +16,47 @@ def not_found(error = None):
     resp = jsonify(message)
     resp.status_code = 404
     return resp
-
 #create user by post method
 @app.route('/postData',methods=['POST'])
 def postData():
     _json = request.json
     name = _json['appName']
     app_description = _json['description']
+    app_icon = _json['app_icon']
     if name and app_description and request.method == 'POST':
-        myCol.insert_one({'appName': name, 'app_description': app_description}) 
+        myCol.insert_one({'appName': name, 'app_description': app_description, 'app_icon': app_icon}) 
         resp = jsonify('user added successfully')
         resp.status_code = 200        
         return resp
     else:
         return not_found()
     
+@app.route('/',methods=['GET'])
+def getAllData():
+    users =  myCol.find()
+    resp = dumps(users)
+    return resp
+
+@app.route('/download_apk')
+def download_apk():
+    # Specify the path to your APK file
+    apk = request.args.get('apk')
+    apk_path = 'assets/apks/'+apk+'.apk'
+    
+    # Define the desired filename for the downloaded APK
+    apk_filename = apk+'.apk'
+    
+    # Use Flask's send_file function to send the file for download
+    return send_file(apk_path, as_attachment=True, download_name=apk_filename)
+
+@app.route('/image')
+def apk_image():
+    image = request.args.get('image')
+    # Specify the path to your APK image file
+    image_path = 'assets/images/'+image+'.png'
+    
+    # Use Flask's send_file function to send the image for display
+    return send_file(image_path, mimetype='image/png')
 
 if __name__ == "__main__":
-    app.run() 
+    app.run(port=8080) 
